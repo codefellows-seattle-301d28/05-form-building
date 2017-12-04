4c048a8125330726336506f252a6395152c1b8fb
billodell <billodell5@gmail.com>
2017-12-03 19:53:34 -0800

    strings working, previews displaying

--- billodell/scripts/articleView.js
+++ billodell/scripts/articleView.js
@@ -79,45 +79,47 @@ articleView.initNewArticlePage = () => {
   // DONE: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
   $('.tab-content').show()
 
-  // TODO: The new articles we create will be copy/pasted into our source data file.
+  // DONE: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
 
   $('#article-json').on('focus', function(){
     this.select();
   });
 
-  // TODO: Add an event handler to update the preview and the export field if any inputs change.
+  // DONE: Add an event handler to update the preview and the export field if any inputs change.
 //  deal with a change event on the form
 };
 // event listener V
 $('#new-form').on('change', function() {
-
+  articleView.create();
 })
 articleView.create = () => {
   // DONE: Set up a variable to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
-
-  // TODO: Instantiate an article based on what's in the form fields:
+  $('#articles').empty();
+  // DONE: Instantiate an article based on what's in the form fields:
   let articleDraft = new Article({
     title: $('#article-title').val(),
     author: $('#article-author').val(),
     category: $('#article-category').val(),
     authorUrl: $('#article-authorUrl').val(),
     body: $('#article-body').val(),
-
+    publishStatus : ($('input[id="article-published"]:checked').length > 0 ? 'Published' : '(draft)'),
     // use the length to see if the checkbox is true or false use a ternary
   })
 
   // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
   // use .toHtml
-  articleDraft.toHtml();
+  $('#articles').append(articleDraft.toHtml())
   // DONE: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
   $('pre code').each(function(i, block){
     hljs.highlightBlock(block);
   });
 
-  // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  // DONE: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
   // use stringify
+  let articleString = JSON.stringify(articleDraft);
+  $('#article-json').val(articleString)
 };
 
 // COMMENT: Where is this function called? Why?
